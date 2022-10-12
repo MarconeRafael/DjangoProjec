1e10f41822ab688571b7f00ff0f16e8b6da47ebd
MarconeRafael <marconerafael6@gmail.com>
2022-10-11 21:45:59 -0300

    melhorando código

--- polls/urls.py
+++ polls/urls.py
@@ -17,12 +17,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', views.post_detail, name='post_login'),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('post/<int:pk>/delete/', views.post_delete, name='post_delete')
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('social-auth/',views.post_detail, name='post_login'),
+
+
 ]
 
 
